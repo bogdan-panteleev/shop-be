ad8bfe418754ac6bcd18c6a42b08402e768a245b
bogdan panteleev <panteleevbbbogdan@gmail.com>
2022-08-23 17:22:58 +0300

    feat: task-3 started renamed service to bahdan-pantsialeyeu-goods-service

--- bahdan-pantsialeyeu-goods-service/serverless.ts
+++ bahdan-pantsialeyeu-goods-service/serverless.ts
@@ -3,7 +3,7 @@ import type { AWS } from '@serverless/typescript';
 import {hello} from '@functions/hello';
 
 const serverlessConfiguration: AWS = {
-  service: 'bahdan-pantsialeyeu-product-service',
+  service: 'bahdan-pantsialeyeu-goods-service',
   frameworkVersion: '3',
   plugins: ['serverless-esbuild'],
   provider: {
